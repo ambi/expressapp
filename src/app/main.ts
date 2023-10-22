9a49f69b23ec0fc81178636fca714f233573e025
tn <prtnog@gmail.com>
2023-10-23 00:32:28 +0900

    add test data

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,10 +1,13 @@
 import { Users } from '../id/repositories/users.js';
 import { createApp, defaultConfig } from './app.js';
+import { createTestData } from './test-data.js';
 
 async function main() {
   const cfg = defaultConfig;
   const users = new Users();
   const app = createApp(cfg, users);
+
+  await createTestData(users);
 
   app.listen(cfg.port, () => {
     console.log(`Example app listening on port ${cfg.port}`);
