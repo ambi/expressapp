b68be8c51f9bf0d48596a4ec7babe5048533e79d
tn <prtnog@gmail.com>
2023-10-25 00:17:01 +0900

    use axios instead of supertest in tests

--- test/app/app.test.ts
+++ test/app/app.test.ts
@@ -1,8 +1,9 @@
+import axios, { CreateAxiosDefaults } from 'axios';
 import * as cheerio from 'cheerio';
 import setCookie from 'set-cookie-parser';
-import supertest from 'supertest';
 import { beforeAll, describe, expect, it } from 'vitest';
 
+import { AddressInfo } from 'net';
 import { createApp, defaultConfig } from '../../src/app/app.js';
 import { createTestData } from '../../src/app/test-data.js';
 import { User } from '../../src/id/models/user.js';
@@ -14,54 +15,69 @@ const users = new Users();
 const { app, logger } = createApp(cfg, users);
 logger.level = LogLevel.SILENT;
 let testData: { users: User[]; userPasswords: string[] };
+const axiosCfg: CreateAxiosDefaults = {
+  validateStatus: () => true, // Don't throw HTTP exceptions.
+  maxRedirects: 0, // No redirects.
+};
 
 beforeAll(async () => {
   testData = await createTestData(users);
+
+  const srv = app.listen(); // Use randomized port in tests.
+  const address = srv.address() as AddressInfo;
+  axiosCfg.baseURL = `http://127.0.0.1:${address.port}`;
 });
 
 describe('GET /signin', () => {
   it('returns the signin page', async () => {
-    const res = await supertest(app).get('/signin');
+    const agent = axios.create(axiosCfg);
+
+    const res = await agent.get('/signin');
     expect(res.status).toBe(200);
-    expect(res.text).toMatch('username');
-    expect(res.text).toMatch('csrf');
+    expect(res.data).toMatch('username');
+    expect(res.data).toMatch('csrf');
   });
 });
 
 describe('GET /home', () => {
   it('redirects to the signin page (unauthenticated)', async () => {
-    const res = await supertest(app).get('/home');
+    const agent = axios.create(axiosCfg);
+
+    const res = await agent.get('/home');
+    console.log(res.data);
     expect(res.status).toBe(302);
     expect(res.headers.location).toEqual('/signin');
   });
 
   it('returns the home page (authenticated)', async () => {
-    let res = await supertest(app).get('/home');
+    const agent = axios.create(axiosCfg);
+
+    let res = await agent.get('/home');
     expect(res.status).toBe(302);
     let location = res.headers.location;
     expect(location).toEqual('/signin');
-    let sCookies = setCookie(res.headers['set-cookie']);
+    let sCookies = setCookie(res.headers['set-cookie']!);
     let cookies = sCookies.map((c) => `${c.name}=${c.value}`).join('; ');
 
-    res = await supertest(app).get(location).set('cookie', cookies);
+    res = await agent.get(location, { headers: { Cookie: cookies } });
     expect(res.status).toBe(200);
-    const dollar = cheerio.load(res.text);
+    const dollar = cheerio.load(res.data);
     const csrfToken = dollar('#signin-csrf').attr('value');
 
-    res = await supertest(app)
-      .post(location)
-      .set('cookie', cookies)
-      .send(`username=${testData.users[0].userName}`)
-      .send(`password=${testData.userPasswords[0]}`)
-      .send(`_csrf=${csrfToken}`);
+    const params = new URLSearchParams({
+      username: testData.users[0].userName,
+      password: testData.userPasswords[0],
+      _csrf: csrfToken!,
+    });
+    res = await agent.post(location, params, { headers: { Cookie: cookies } });
     expect(res.status).toBe(302);
     location = res.headers.location;
     expect(location).toEqual('/home');
-    sCookies = setCookie(res.headers['set-cookie']);
+    sCookies = setCookie(res.headers['set-cookie']!);
     cookies = sCookies.map((c) => `${c.name}=${c.value}`).join('; ');
 
-    res = await supertest(app).get(location).set('cookie', cookies);
+    res = await agent.get(location, { headers: { Cookie: cookies } });
     expect(res.status).toBe(200);
-    expect(res.text).toMatch(`User name: ${testData.users[0].userName}`);
+    expect(res.data).toMatch(`User name: ${testData.users[0].userName}`);
   });
 });
